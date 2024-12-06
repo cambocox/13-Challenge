abf98e32a4985dfae5e79dad06bc9a0e4343d5bd
Camden Cox <camdencox93@gmail.com>
2024-12-05 22:12:13 -0500

    Updated CandidateSearch.tsx and Saved Candidates.tsx

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,9 +1,13 @@
+import SavedCandidateList from '../components/SavedCandidateList';
+
 const SavedCandidates = () => {
   return (
     <>
       <h1>Potential Candidates</h1>
+      <SavedCandidateList />
     </>
   );
 };
 
 export default SavedCandidates;
+
